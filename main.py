66f824a5653139a3016d46164f484edd79025d8c
ja0919a <105212631+ja0919a@users.noreply.github.com>
2023-06-20 19:08:50 +0800

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#遊戲主要入口
+from source import tools, setup
+
+def main():
+    game = tools.Game()
+    game.run()
+
+if __name__ == '__main__':
+    main()
---
+++ source/constants.py
@@ -0,0 +1,4 @@
+#遊戲的常量
+screen_width = 800
+screen_height = 600
+screen_size = (screen_width, screen_height)
